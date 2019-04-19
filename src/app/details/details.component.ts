c85723e42db9a6473ab43f841b2c31c4e2d53366
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-04-19 17:59:27 +0200

    correct details css file path

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,7 +6,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit, OnDestroy {
 
