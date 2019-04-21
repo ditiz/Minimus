ea9c45956dcff2d2faeeedfa52fa467372b2363e
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-04-22 00:58:40 +0200

    add dark mode on weather cards

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../ui.service';
 
 @Component({
   selector: 'app-weather-card',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherCardComponent implements OnInit {
 
-  constructor() { }
+  darkModeActive: boolean;
+
+  constructor(public ui: UiService) { }
 
   ngOnInit() {
+    this.ui.darkModeState.subscribe((value) => {
+      this.darkModeActive = value;
+    })
   }
 
 }
