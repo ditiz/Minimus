8ab80b1cc387b506541892e66977b90529248873
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-04-23 21:53:26 +0200

    add http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { WeatherService } from './weather.service'
+import { UiService } from './ui.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,9 +11,6 @@ import { HomeComponent } from './home/home.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { AddCardComponent } from './add-card/add-card.component';
 import { DetailsComponent } from './details/details.component';
-
-import { WeatherService } from './weather.service'
-import { UiService } from './ui.service';
 import { DarkModeToggleComponent } from './dark-mode-toggle/dark-mode-toggle.component';
 
 @NgModule({
@@ -23,7 +24,8 @@ import { DarkModeToggleComponent } from './dark-mode-toggle/dark-mode-toggle.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     WeatherService,
